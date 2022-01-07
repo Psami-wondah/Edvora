a14ec9baae604b6edd3d75b33f8303671d20bdf7
Psami-wondah <okechukwusamuel16@gmail.com>
2022-01-07 09:14:59 +0100

    truncate to 50

--- Backend/urls/room.py
+++ Backend/urls/room.py
@@ -19,14 +19,14 @@ class RoomResponse(BaseModel):
     room: RoomCreate
 
 
-@room.post('/api/v1/room/create', response_model=RoomResponse)
-async def create_room(room: Room, current_user: User = Depends(get_current_user)):
-    if db.rooms.find_one({"room_name": room.room_name}):
-        return JSONResponse({"message": "room name already exists"}, status_code=status.HTTP_208_ALREADY_REPORTED)
-    data = {
-        'creator': current_user.username,
-        **room.dict(),
-        'short_id': str(generate_short_id())
-    }
-    db.rooms.insert_one(data)
-    return {"message": "room created", "room": data}
+# @room.post('/api/v1/room/create', response_model=RoomResponse)
+# async def create_room(room: Room, current_user: User = Depends(get_current_user)):
+#     if db.rooms.find_one({"room_name": room.room_name}):
+#         return JSONResponse({"message": "room name already exists"}, status_code=status.HTTP_208_ALREADY_REPORTED)
+#     data = {
+#         'creator': current_user.username,
+#         **room.dict(),
+#         'short_id': str(generate_short_id())
+#     }
+#     db.rooms.insert_one(data)
+#     return {"message": "room created", "room": data}
